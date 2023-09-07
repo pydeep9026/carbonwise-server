6775d4df620aadad7e3926e754b1266765272b89
pradeep singh <pradeepsi2120032@gmail.com>
2023-09-07 21:43:51 +0530

    testing

---
+++ index.js
@@ -0,0 +1,45 @@
+const express=require("express")
+const cors=require("cors")
+const mongoose=require("mongoose")
+const bodyParser = require('body-parser');
+
+const app=express()  
+require("dotenv").config()
+
+app.use(bodyParser.json({ limit: "200mb" }));
+app.use(bodyParser.urlencoded({ limit: "200mb",  extended: true, parameterLimit: 1000000 }));
+app.use(cors()) 
+app.use(express.json());   
+  
+  
+  
+ 
+
+
+const PORT=process.env.PORT || 5000
+
+const dburl='mongodb://0.0.0.0:27017/chat'
+/*const dburl='mongodb+srv://PRADEEP123:'+encodeURIComponent('password')+'@cluster0.x5jbf4w.mongodb.net/chat?retryWrites=true&w=majority'*/
+/*const dburl=process.env.db_url*/
+ 
+
+mongoose.connect(dburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true       
+  });
+  
+  mongoose.connection.on('connected', () => {    
+    console.log('Successfully connected to MongoDB');  
+
+    mongoose.connection.db.command({ serverStatus: 1 }, (err, result) => {
+        if (err) throw err;
+        console.log(`Number of live connections: ${result.connections.current}`);
+    });     
+  });
+  
+
+
+const server=app.listen(process.env.PORT,()=>{
+    console.log(`server started at port ${PORT}`)
+})  
+
